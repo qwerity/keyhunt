c3724a4eba60bef52a8db263e7599c7a868bb88e
Khachik Shahzadyan <khachik@teramind.co>
2024-10-16 02:06:22 +0400

    Add error logging to aes enc/dec

--- util/encryption.cpp
+++ util/encryption.cpp
@@ -3,38 +3,67 @@
 
 #include <openssl/evp.h>
 #include <openssl/aes.h>
+#include <openssl/err.h>
+
+#include <boost/log/trivial.hpp>
 
 namespace crypto
 {
+    void logOpenSSLErrors()
+    {
+        unsigned long errCode;
+        std::ostringstream oss;
+
+        while ((errCode = ERR_get_error()) != 0)
+        {
+            char errBuffer[256];
+            ERR_error_string_n(errCode, errBuffer, sizeof(errBuffer));
+            oss << "OpenSSL Error: " << errBuffer << '\n';
+        }
+
+        const std::string errString = oss.str();
+        if (!errString.empty())
+        {
+            BOOST_LOG_TRIVIAL(error) << errString;
+        }
+    }
+
     bool aes_gcm_enc(const std::string& plaintext, const std::vector<uint8_t>& key, const std::vector<uint8_t>& iv, std::vector<uint8_t>& tag, std::vector<uint8_t>& ciphertext)
     {
         EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
-        if (!ctx) { return false; }
+        if (!ctx)
+        {
+            logOpenSSLErrors();
+            return false;
+        }
         utils::ScopeOutRunner scopeOutRunner([&ctx]() { EVP_CIPHER_CTX_free(ctx); });
 
         if (EVP_EncryptInit_ex(ctx, EVP_aes_256_gcm(), nullptr, key.data(), iv.data()) != 1)
         {
+            logOpenSSLErrors();
             return false;
         }
 
         int len;
         ciphertext.resize(plaintext.size() + AES_BLOCK_SIZE);
-        if (EVP_EncryptUpdate(ctx, ciphertext.data(), &len, reinterpret_cast<const uint8_t*>(plaintext.data()), plaintext.size()) != 1)
+        if (EVP_EncryptUpdate(ctx, ciphertext.data(), &len, reinterpret_cast<const uint8_t*>(plaintext.data()), static_cast<int>(plaintext.size())) != 1)
         {
+            logOpenSSLErrors();
             return false;
         }
         int ciphertext_len = len;
 
         if (EVP_EncryptFinal_ex(ctx, ciphertext.data() + len, &len) != 1)
         {
+            logOpenSSLErrors();
             return false;
         }
         ciphertext_len += len;
         ciphertext.resize(ciphertext_len);
 
-        tag.resize(16);
-        if (EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_GET_TAG, tag.size(), tag.data()) != 1)
+        if (EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_GET_TAG, static_cast<int>(tag.size()), tag.data()) != 1)
         {
+            logOpenSSLErrors();
             return false;
         }
 
@@ -44,29 +73,37 @@ namespace crypto
     bool aes_gcm_dec(const std::vector<uint8_t>& ciphertext, const std::vector<uint8_t>& key, const std::vector<uint8_t>& iv, const std::vector<uint8_t>& tag, std::string& plaintext)
     {
         EVP_CIPHER_CTX* ctx = EVP_CIPHER_CTX_new();
-        if (!ctx) { return false; }
+        if (!ctx)
+        {
+            logOpenSSLErrors();
+            return false;
+        }
         utils::ScopeOutRunner scopeOutRunner([&ctx]() { EVP_CIPHER_CTX_free(ctx); });
 
         if (EVP_DecryptInit_ex(ctx, EVP_aes_256_gcm(), nullptr, key.data(), iv.data()) != 1)
         {
+            logOpenSSLErrors();
             return false;
         }
 
         std::vector<uint8_t> plaintext_buffer(ciphertext.size());
         int len;
-        if (EVP_DecryptUpdate(ctx, plaintext_buffer.data(), &len, ciphertext.data(), ciphertext.size()) != 1)
+        if (EVP_DecryptUpdate(ctx, plaintext_buffer.data(), &len, ciphertext.data(), static_cast<int>(ciphertext.size())) != 1)
         {
+            logOpenSSLErrors();
             return false;
         }
         int plaintext_len = len;
 
-        if (EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, tag.size(), const_cast<uint8_t*>(tag.data())) != 1)
+        if (EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_GCM_SET_TAG, static_cast<int>(tag.size()), const_cast<uint8_t*>(tag.data())) != 1)
         {
+            logOpenSSLErrors();
             return false;
         }
 
         if (EVP_DecryptFinal_ex(ctx, plaintext_buffer.data() + len, &len) != 1)
         {
+            logOpenSSLErrors();
             return false;
         }
         plaintext_len += len;
@@ -83,6 +120,7 @@ namespace crypto
 
     bool AES::encrypt(const std::string& plaintext, std::vector<uint8_t>& tag, std::vector<uint8_t>& ciphertext) const
     {
+        tag.resize(tagSize);
         return aes_gcm_enc(plaintext, mKey, mIV, tag, ciphertext);
     }
 
