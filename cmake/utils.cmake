5a8777499f91205ea10420f9ae64e3a671ce77f1
Khachik Shahzadyan <khachik@teramind.co>
2024-10-16 02:00:24 +0400

    Update warnings to suspend

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -6,5 +6,7 @@ function(suppress_msvc_warnings target)
         target_compile_options(${target} PRIVATE /wd4365)  # Signed/unsigned mismatch
         target_compile_options(${target} PRIVATE /wd4820)  # Padding added after data member
         target_compile_options(${target} PRIVATE /wd4868)  # C4868: compiler may not enforce left-to-right evaluation order in braced initializer list
+        target_compile_options(${target} PRIVATE /wd4710)  # C4710: function not inlined
+        target_compile_options(${target} PRIVATE /wd4711)  # C4711: selected for automatic inline expansion
     endif()
 endfunction()
